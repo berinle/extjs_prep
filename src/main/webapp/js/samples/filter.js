91d440a2b62d238446539ac81c1cd767bab8da9e
Bayo Erinle <berinle@gmail.com>
2011-08-24 16:18:58 -0400

    toying with multiselects

---
+++ src/main/webapp/js/samples/filter.js
@@ -0,0 +1,68 @@
+Ext.onReady(function(){
+    Ext.QuickTips.init();
+    Ext.form.Field.prototype.msgTarget = 'side';
+
+
+    //todo grab from database
+    var filterDs = new Ext.data.ArrayStore({
+        data: [
+            [1, 'Birth Place'],
+            [2, 'Citizenship'],
+            [3, 'Gender'],
+            [4, 'Felony'],
+            [5, 'Conviction'],
+            [6, 'Misdemeanor'],
+            [7, 'Limitations'],
+            [8, 'Photograph']
+        ],
+        fields: ['value','text'],
+        sortInfo:{ field:'value', direction:'ASC' }
+
+    });
+
+    var filterForm = new Ext.form.FormPanel({
+        title:'',
+        width:600,
+        renderTo:'filterSelector',
+        items:[
+            {
+                xtype:'itemselector',
+                name:'itemselector',
+                fieldLabel:'',
+                imagePath:'../js/ext/examples/ux/images',
+                multiselects:[
+                    {
+                        legend:'Screened',
+                        width:200,
+                        height:200,
+                        store:filterDs,
+                        displayField:'text',
+                        valueField:'value'
+                    },
+                    {
+                        legend:'Viewable',
+                        width:200,
+                        height:200,
+                        store:[]
+                        /*,tbar:[{
+                            text:'clear',
+                            handler:function(){
+                                filterForm.getForm().findField('itemselector').reset();
+                            }
+                        }]*/
+
+                    }]
+            } ],
+        buttons: [{
+            text: 'Apply',
+            handler:function(){
+                if(filterForm.getForm().isValid()){
+                    console.log('submitted values: ' + filterForm.getForm().getValues(true));
+                }
+            }}, {
+            text: 'Cancel', handler: function(){ }
+        }]
+
+    });
+
+});
--- src/main/webapp/panels/filter.jsp
+++ src/main/webapp/panels/filter.jsp
@@ -1,26 +1,30 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
 <html>
-<head><title>Report Panel</title>
-        <link rel="stylesheet" type="text/css"
-    href="../js/extjs/resources/css/ext-all.css" />
-    <script type="text/javascript" src="../js/extjs/adapter/ext/ext-base.js">
-    </script>
-    <script type="text/javascript" src="../js/extjs/ext-all-debug.js">
-    </script>
+<head>
+    <title>Report Panel</title>
+    <link rel="stylesheet" type="text/css" href="../js/ext/resources/css/ext-all.css"/>
+    <script type="text/javascript" src="../js/ext/ext-base.js"></script>
+    <script type="text/javascript" src="../js/ext/ext-all-debug.js"></script>
 
-    <script type="text/javascript">
+    <script type="text/javascript" src="../js/ext/examples/ux/MultiSelect.js"></script>
+    <script type="text/javascript" src="../js/ext/examples/ux/ItemSelector.js"></script>
 
-        Ext.onReady(function() {
 
-        });
+    <script type="text/javascript" src="../js/samples/filter.js"></script>
 
-    </script>
 
 </head>
 
 <body>
 <!-- filled in by ExtJS -->
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<div id="filterSelector"></div>
+
 </body>
 
 
